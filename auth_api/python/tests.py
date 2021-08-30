6ea5f38654021933c01d8e44c4f4228b9cd6e516
marcos-julian <62956234+marcos-julian@users.noreply.github.com>
2021-08-29 21:19:53 -0500

    Update tests.py

--- auth_api/python/tests.py
+++ auth_api/python/tests.py
@@ -9,10 +9,14 @@ class TestStringMethods(unittest.TestCase):
         self.validate = Restricted()
 
     def test_generate_token(self):
-        self.assertEqual('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYWRtaW4ifQ.StuYX978pQGnCeeaj2E1yBYwQvZIodyDTCJWXdsxBGI', self.convert.generate_token('admin', 'secret'))
+        self.assertEqual('eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhZG1pbiI6InNlY3JldCJ9.mpOWaiHGnJ-7RX9K3IhEsNFVxT3tIJUQLhEjzz4vhuo', self.convert.generate_token('admin', 'secret'))
+        self.assertEqual('eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJub2FkbWluIjoibm9Qb3czciJ9.NRIMXo9raefsgrJuGsAgjRIGJNVg92bvW3sXbZoTEp8', self.convert.generate_token('noadmin', 'noPow3r'))
+        self.assertEqual('eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJib2IiOiJ0aGlzSXNOb3RBUGFzc3dvcmRCb2IifQ.hYEbsn-66EsG7qEOUmK3tLhXc7Gj-Pv5AKnOToM2RuE', self.convert.generate_token('bob', 'thisIsNotAPasswordBob'))
 
     def test_access_data(self):
-        self.assertEqual('You are under protected data', self.validate.access_data('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYWRtaW4ifQ.StuYX978pQGnCeeaj2E1yBYwQvZIodyDTCJWXdsxBGI'))
+        self.assertEqual('You are under protected data', self.validate.access_data('eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhZG1pbiI6InNlY3JldCJ9.mpOWaiHGnJ-7RX9K3IhEsNFVxT3tIJUQLhEjzz4vhuo'))
+        self.assertEqual('You are under protected data', self.validate.access_data('eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJub2FkbWluIjoibm9Qb3czciJ9.NRIMXo9raefsgrJuGsAgjRIGJNVg92bvW3sXbZoTEp8'))
+        self.assertEqual('You are under protected data', self.validate.access_data('eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJib2IiOiJ0aGlzSXNOb3RBUGFzc3dvcmRCb2IifQ.hYEbsn-66EsG7qEOUmK3tLhXc7Gj-Pv5AKnOToM2RuE'))
 
 if __name__ == '__main__':
     unittest.main()
